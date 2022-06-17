f4acdf5e7063d8f31b79e3fb139d3eccfdcfb4ab
Pantarat Vichathai <>
2022-06-17 15:15:39 +0700

    no message

--- Adapter_Agent/test.js
+++ Adapter_Agent/test.js
@@ -1,18 +1,13 @@
 const adapter = require('./adapter_tools/writeTo_newDatabase');
-//environment
-require('dotenv').config({path: './database_props.env'});
-let host = process.env.HOST;
-let user = process.env.USER;
-let password = process.env.PASSWORD;
-let readdatabase = process.env.READDATABASE;
-let writedatabase = process.env.WRITEDATABASE;
-
-//Need to change connection properties here
-adapter.makeReadConnection([host, user, password, readdatabase]);
-adapter.makeWriteConnection([host, user, password, writedatabase]);
+//Need to change connection properties in environment '.env' file
+//template for .env file
+// HOST            =
+// DBUSER          =
+// PASSWORD        = 
+// READDATABASE    = 
+// WRITEDATABASE   = 
 
 adapter.pullAllObject_Pop();
-
 adapter.pullAllVisitor_Log();
 
 
