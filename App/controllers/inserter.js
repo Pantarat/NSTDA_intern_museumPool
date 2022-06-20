8a676dc01554ee8525db9eb1fde5cf7f9a2bfede
Supasake Vongvipaporn <6322771294@g.siit.tu.ac.th>
2022-06-21 02:58:17 +0700

    App version 0.1 update some UI

--- App/controllers/inserter.js
+++ App/controllers/inserter.js
@@ -1,19 +1,23 @@
 const model = require('../models/insert')
 module.exports ={
     main : function(req, res, next) {
-        res.render('insert');
+        const feedback = req.flash('feedback')
+
+        res.render('insert',{feedback});
     },
     insert_form : function(req, res, next) {
         res.render('insert_one_form');
     },
     insert_One : function(req, res, next) {
         model.insert_one_model(req)
+        req.flash('feedback','Insert one Completed')
         res.redirect('/insert');//redirectกลับไปหน้าริ่ม   
     },
     insert_Update : function(req, res, next) {
         console.log('done55');
         model.insert_update_model;
-        res.redirect('/');
+        req.flash('feedback','Update Completed')
+        res.redirect('/insert');
     }
 
 }
