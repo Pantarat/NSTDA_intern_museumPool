fc76b7010cabdb560096c25c5c01c267b1ecd1b6
Pantarat Vichathai <>
2022-06-21 13:13:03 +0700

    no message

--- App/models/popular_model.js
+++ App/models/popular_model.js
@@ -1,8 +1,17 @@
 const mysql_tools = require('./mysql_tools');
-const mysql = require('mysql');
-const dfd = require('danfojs-node');
-const {dbogCon: dbogCon, dbupdCon: dbupdCon} = require('../config/db');
-const allexports = require('./writeTo_newDatabase');
+
+function value_count(input = []){
+    let output = {}
+    for (let value of input){
+        if (Object.keys(output).includes(value)){
+            output[value] += 1;
+        }
+        else {
+            output[value] = 1;
+        }
+    }
+    return output;
+}
 
 async function process_popular(){
 
@@ -21,11 +30,12 @@ async function process_popular(){
         row['VisitorLog_UpdDate'] = Date.parse(row['VisitorLog_UpdDate']);
         }
     )
-    console.log(pop_data)
+    //console.log(pop_data)
 
 
 }
 
 let allfuncs = {};
 allfuncs.process_popular = process_popular;
+allfuncs.value_count = value_count;
 module.exports = allfuncs;
--- App/test.js
+++ App/test.js
@@ -1,4 +1,5 @@
 write = require('./models/writeTo_newDatabase');
 test = require('./models/popular_model');
 //write.pullAllVisitor_Log();
-test.process_popular();
+//console.log(test.process_popular());
+console.log(test.value_count(['a','b','c','a','b','b','b','b']));
