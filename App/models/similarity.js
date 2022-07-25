06cc9188a49af5ab1bf316b1e027be446dd0873f
Pantarat Vichathai <>
2022-07-25 12:28:31 +0700

    similarity v4

--- App/models/similarity.js
+++ App/models/similarity.js
@@ -155,90 +155,85 @@ async function writeAllSimilarity(startfrom = 0) {
     return 0;
 }
 
-async function getSimilarObject_Code(visitor_id,limit = 20) {
+async function getSimilarObject_Code(visitor_id, limit = 20) {
     if (!visitorIDs) {
         visitorIDs = await getAllVisitorID();
     }
-    if (!visitorIDs.includes(visitor_id)){
+    if (!visitorIDs.includes(visitor_id)) {
         console.log('Could not find visitor id :', startfrom);
         return 0;
     }
-    let sim_data = await sql_tool.getAllData(process.env.UPDATEDDB,'visitor_similarity',`visitor_id = ${visitor_id}`);
+    let sim_data = await sql_tool.getAllData(process.env.UPDATEDDB, 'visitor_similarity', `visitor_id = ${visitor_id}`);
     let similarVisitors = Object.keys(JSON.parse(sim_data[0].similarity));
     let allSimilarObjects = [];
-    for (let i=0; i<similarVisitors.length; i++){
+    for (let i = 0; i < similarVisitors.length; i++) {
         allSimilarObjects = allSimilarObjects.concat(Array.from(await getVisitedObjectCodes(parseInt(similarVisitors[i]))));
     }
     let simObjectSorted = {};
     allSimilarObjects.forEach(object => {
-        if (Object.keys(simObjectSorted).includes(object)){
+        if (Object.keys(simObjectSorted).includes(object)) {
             simObjectSorted[object] += 1;
         }
-        else{
+        else {
             simObjectSorted[object] = 1;
         }
     })
-    let mostCommonObject = Object.entries(simObjectSorted).sort((x, y) => y[1] - x[1]).slice(0,limit);
+    let mostCommonObject = Object.entries(simObjectSorted).sort((x, y) => y[1] - x[1]).slice(0, limit);
     return mostCommonObject;
 }
 
-async function getRecommedImage(visitor_id, limit = 20) {
-    let mostCommonObject = await getSimilarObject_Code(visitor_id,limit);
-    return new Promise((resolve,reject)=>{
-        let mostCommonObjectArr = mostCommonObject.map(x=>{ return x[0]});
-        sql_tool.getColumnArrayOfData(process.env.UPDATEDDB,'object_image',['Object_ID','ImageObject_Path'],`Object_Code IN (${mostCommonObjectArr})`)
-        .then(recommend =>{
-            console.log(recommend);
-            resolve(recommend);
-        })
-        .catch(err => {
-            reject(err);
+async function getRecommedImage(visitor_id, limit = 20, distance = 1000) {
+    let mostCommonObject = await getSimilarObject_Code(visitor_id);
+    return new Promise((resolve, reject) => {
+        let mostCommonObjectArr = mostCommonObject.map(x => { return x[0] });
+        let queryString = ` SELECT od.id,od.image,(
+            -- for the distance
+            111.111 *
+            DEGREES(ACOS(LEAST(1.0, COS(RADIANS(od.place_latitude))
+             * COS(RADIANS(mostrecent.place_latitude))
+             * COS(RADIANS(od.place_longtitude - mostrecent.place_longtitude))
+             + SIN(RADIANS(od.place_latitude))
+             * SIN(RADIANS(mostrecent.place_latitude)))))) AS distance_in_km
+             -- for the distance
+            FROM object_description od
+            JOIN visitor_log v ON od.object_code = v.Object_Code
+            JOIN (
+                SELECT Object_Code,place_latitude,place_longtitude
+                FROM object_description
+                WHERE Object_Code =
+                    (SELECT Object_Code
+                    from visitor_log
+                    where Visitor_ID=${visitor_id}
+                    group by Object_Code
+                    order by max(VisitorLog_UpdDate) DESC Limit 1)) AS mostrecent
+            WHERE od.object_code NOT IN(
+                SELECT object_code
+                FROM visitor_log
+                WHERE Visitor_ID = ${visitor_id} -- filter out objects thats already seen
+                )
+            AND
+                (v.VisitorLog_UpdDate
+                BETWEEN DATE(NOW() - INTERVAL 6 MONTH)
+                AND DATE(NOW()))
+            AND od.Object_Code IN (${mostCommonObjectArr})
+            GROUP BY od.id
+            -- for the distance
+            HAVING distance_in_km < ${distance}
+            -- for the distance
+            ORDER BY distance_in_km ASC
+            LIMIT ${limit}`;
+
+        dbupdCon.query(queryString, (err, result) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                console.log(JSON.parse(JSON.stringify(result)));
+                resolve(result);
+            }
         })
     })
 }
-
-//Slower version, tested by pulling all data first
-// async function writeAllSimilarity2(startfrom = 0) {
-//     console.time(`Time to get all data`);
-//     let alldata = await sql_tool.getColumnArrayOfData(process.env.UPDATEDDB, `visitor_log`, ['Visitor_ID', 'Object_Code']);
-//     let allVisitorID = Array.from(new Set(alldata.map((x) => { return x.Visitor_ID })));
-//     let startfromind = 0;
-//     //turn the startfrom id into index.
-//     if (startfrom > 0) {
-//         if (!allVisitorID.includes(startfrom)) {
-//             console.log('Could not find visitor id :', startfrom);
-//             return 0;
-//         }
-//         else {
-//             startfromind = allVisitorID.indexOf(startfrom);
-//         }
-//     }
-//     console.timeEnd(`Time to get all data`);
-//     //the for loop
-//     let queryString = `INSERT INTO visitor_similarity (visitor_id,similarity)
-//     VALUES (?,?)
-//     ON DUPLICATE KEY
-//     UPDATE similarity = VALUES(similarity);`
-
-//     for (let i = startfromind; i < allVisitorID.length; i++) {
-//         let relationJSON = {};
-//         let id = allVisitorID[i];
-//         console.time(`write Similarity Time for id:${id}`); // Timed the process
-//         let data1 = new Set(alldata.filter(log => log.Visitor_ID === id).map((x) => { return x.Object_Code }));
-//         for (let j = 0; j < allVisitorID.length; j++) {
-//             let otherid = allVisitorID[j];
-//             let data2 = new Set(alldata.filter(log => log.Visitor_ID === otherid).map((x) => { return x.Object_Code }));
-//             if (id != otherid) {
-//                 relationJSON[otherid] = await findSimilarity(data1, data2);
-//             }
-//             //console.log('Got relation upto ',otherid);
-//         }
-
-//         await sql_tool.writeColumn('visitor_similarity', ['visitor_id', 'similarity'], [id, JSON.stringify(relationJSON)]);
-//         console.log('Written similarity completed for id = ', id);
-//         console.timeEnd(`write Similarity Time for id:${id}`);
-//     }
-// }
 
 similarityexports = {}
 similarityexports.getVisitedObjectCodes = getVisitedObjectCodes;
@@ -248,7 +243,6 @@ similarityexports.findSimilarity = findSimilarity;
 similarityexports.writeSimilarityForID = writeSimilarityForID;
 similarityexports.writeAllSimilarity = writeAllSimilarity;
 similarityexports.findSimilarityByID = findSimilarityByID;
-//similarityexports.writeAllSimilarity2 = writeAllSimilarity2;
 similarityexports.getSimilarObject_Code = getSimilarObject_Code;
 similarityexports.getRecommedImage = getRecommedImage;
 
